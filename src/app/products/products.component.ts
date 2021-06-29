5208b59933a8fa780d5eacc8cbbb08d01f02022c
oldrover <oldrover@web.de>
2021-06-29 13:10:01 +0200

    created products component

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/Product';
+import { ProductService } from '../services/product.service';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+  title: string = "Products";
+  products: Product[] = [];
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe(products => {
+      this.products = products;
+    })
+
+  }
+
+}
