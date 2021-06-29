69942bf9a558070380259b2e07e7f846294eccd6
oldrover <oldrover@web.de>
2021-06-29 13:09:26 +0200

    created product model

---
+++ src/app/models/Product.ts
@@ -0,0 +1,18 @@
+export class Product {
+    id: number;
+    title: string;
+    price: number;
+    description: string;
+    category: string;
+    image: string;
+
+    constructor() {
+        this.id = 1;
+        this.title = '';
+        this.price = 0;
+        this.description = '';
+        this.category = '';
+        this.image = '';
+    }
+
+}
