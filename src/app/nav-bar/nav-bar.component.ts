bcbd1f88a1c86c90cdf57ff03c50bfa45ea1f5a9
oldrover <oldrover@web.de>
2021-07-01 09:20:49 +0200

    changed cart icon

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
+  faCart = faShoppingCart;
 
   constructor() { }
 
