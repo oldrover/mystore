0bbe24acfe18cd7fbf586e06e6a6616b0fb5074c
oldrover <oldrover@web.de>
2021-07-01 09:22:41 +0200

    created shopping cart list

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -9,10 +9,24 @@ import { CartService } from '../services/cart.service';
 })
 export class ShoppingCartComponent implements OnInit {
   products = this.cartService.getCartProducts();
+  total: number = 0;
+  empty: boolean = true;
 
   constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
+    if (this.products.length > 0) {
+      this.total = this.products.reduce((prev, cur) => {
+        return prev + cur.price;
+      }, 0);
+      this.empty = false;
+    }
+    else {
+
+    }
+
+    
+
    
   }
 
